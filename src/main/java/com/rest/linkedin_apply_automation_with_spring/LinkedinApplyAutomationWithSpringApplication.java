b9e4d93666214b6f84180209ead3631864610f93
Vinícius Andrade <vinicius_andrade2010@hotmail.com>
2025-11-18 11:00:02 -0300

    initialize Spring Boot project with Maven configuration and basic structure

---
+++ src/main/java/com/rest/linkedin_apply_automation_with_spring/LinkedinApplyAutomationWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rest.linkedin_apply_automation_with_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinApplyAutomationWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinApplyAutomationWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/linkedin_apply_automation_with_spring/LinkedinApplyAutomationWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.linkedin_apply_automation_with_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinApplyAutomationWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
